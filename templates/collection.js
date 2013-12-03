ac3f062432d5584175b6e88e79e553c3e5d3591d
stefanbuck <web@broud.de>
2013-12-03 23:47:33 +0100

    add comments

--- templates/collection.js
+++ templates/collection.js
@@ -2,6 +2,16 @@
 
 <%= _.camelize(appname) %>.Collections = <%= _.camelize(appname) %>.Collections || {};
 
+/**
+ * A collection is just a list of models and even more.
+ * It contains a bunch of helper functions, sync logic
+ * and events.
+ *
+ * For further information go to:
+ * http://the-m-project.org/docs/absinthe/Collection.html
+ * http://backbonejs.org/#Collection
+ */
+
 (function() {
     'use strict';
 
--- templates/layout.js
+++ templates/layout.js
@@ -2,6 +2,14 @@
 
 <%= _.camelize(appname) %>.Layouts = <%= _.camelize(appname) %>.Layouts || {};
 
+/**
+ * A layout provides an easy way to switch between different
+ * views with similar or different content.
+ *
+ * For further information go to:
+ * http://the-m-project.org/docs/absinthe/Layout.html
+ */
+
 (function() {
     'use strict';
 
